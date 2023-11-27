d63f551ae0c3a0ec71060bb4fa981eb8acd64d2b
michael-junior <michaeljunioroliveira@gmail.com>
2023-11-27 00:16:13 -0300

    Implementado redirecionamento da pagina e tratamento de exceções de redirecionamento

--- webdev/tarefas/views.py
+++ webdev/tarefas/views.py
@@ -1,8 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
+from webdev.tarefas.forms import TarefaNovaForm
 
-# Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        form = TarefaNovaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('tarefas:home'))
+        else:
+            return render(request, 'tarefas/home.html', {'form': form}, status=400)
     return render(request, 'tarefas/home.html')
